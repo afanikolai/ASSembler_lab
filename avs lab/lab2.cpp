17724b1a1d8269b5f1e96ff2ecaf90c2838618e2
afanikolai <nik-af-01@yandex.ru>
2021-09-19 00:22:01 +0500

    Добавьте файлы проекта.

---
+++ avs lab/lab2.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+/*
+В массиве хранятся числа со знаком. Найти максимальное значение и
+максимальное по модулю значение, а также индексы этих значений. Если это один
+и тот же элемент, то занести в переменную S единицу (иначе 0). [2D] Аналогичным
+образом занести 1 в переменную N, если найденные максимумы находятся в
+соседних столбцах или строках (иначе 0) Считать, что указанные максимальные
+значения в массиве не повторяются.
+*/
+
+const int INF = 1e9;
+const int Narr = 5;
+const int Marr = 5;
+
+int main()
+{
+    srand(time(0));
+    int n = Narr, m = Marr;
+    int arr[Narr][Marr];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            arr[i][j] = rand() % 2000 - 1000;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //ma - max elem, mx - x max elem x, my - y max elem y
+    //abs - |max elem| (abs), absx - x max elem abs x, absy - y max elem abs y
+    int ma = -1000, mx, my, abs = -1, absx, absy, S = 0, N = 0, tmp;
+    __asm {
+        jmp start
+
+        MaxEl :  // Найти максимальный элемент. 
+            lea esi, arr
+            mov ecx, n  // Номер строки 
+            forArrN :
+                mov tmp, ecx    // Сохраняем текущий номер строки. 
+                push ecx
+                mov ecx, m
+                forArrM :
+                    mov eax, [esi]
+                    cmp eax, ma // if arr[i] > max
+                    jg Swap 
+                    jmp nex // Конец итерации.
+                    Swap :  // Cохраняем максимальный элемент и его поизицию.
+                        mov eax, [esi]
+                        mov ma, eax     // Сохраняем максимум. 
+                        mov ebx, tmp
+                        mov mx, ebx     // Сохраняем индексы максимума.
+                        mov my, ecx
+                    nex :
+                    add esi, 4d // Переход к следующему элементу массива. 
+                loop forArrM
+                pop ecx // Достаем из стека n.
+            loop forArrN
+        ret
+
+        MaxElAbs :
+            lea esi, arr
+            mov ecx, n  // Номер строки 
+            forArrNAbs :
+                mov tmp, ecx    // Сохраняем текущий номер строки. 
+                push ecx
+                mov ecx, m
+                forArrMAbs :
+                    mov eax, [esi]
+                    cmp eax, 0d // if arr[i] > 0
+                    jg pos
+                    neg eax // Сохраняем модуль arr[i] в еах
+                    pos :
+
+                    cmp eax, abs    // if arr[i] > текущего максимума.
+                    jg SwapAbs
+                    jmp nexAbs  // Переход к концу итерации
+                    SwapAbs :   // Cохраняем максимальный элемент и его поизицию.
+                        mov eax, [esi]
+                        cmp eax, 0d // if arr[i] > 0
+                        jg pos2
+                            neg eax // Сохраняем модуль arr[i] в еах
+                        pos2 : 
+                            mov abs, eax    // Сохраняем максимум. 
+                            mov ebx, tmp
+                            mov absx, ebx   // Сохраняем индексы максимума.
+                            mov absy, ecx
+                    nexAbs :
+                    add esi, 4d // Переход к следующему элементу массива. 
+                loop forArrMAbs
+                pop ecx // Достаем из стека n.
+            loop forArrNAbs
+        ret
+
+        start : // Основная программа, "функция Main"
+
+            // Обнуляем переменные.
+            xor eax, eax
+            xor ebx, ebx
+            xor edx, edx
+
+            // Находим максимальный элемент.
+            call MaxEl 
+
+            // Находим максимальный элемент по модулю.
+            call MaxElAbs
+
+            mov eax, ma     // Записываем в eax максимальный элемент.
+            mov ebx, abs    // Записываем в ebx максимальный по модулю элемент.
+
+            cmp eax, ebx    // if макс. элемент != макс. по модулю элементу.
+            jne NotEqual 
+                mov S, 1d   // S = 1.
+
+            NotEqual :
+            mov eax, mx     // Записываем в eax позицию по x максимального элемента.
+            mov ebx, absx   // Записываем в ebx позицию по x максимального элемента по модулю.
+            sub eax, ebx    // Вычитание для опрделения , в соседних ли строках.
+                cmp eax, 0  // Проверка знака, чтобы узнать, нужно ли брать модуль.
+                jg Greater
+                neg eax     // Модуль.
+            Greater :
+            cmp ax, 1   // if |ax| > 1, то проверяем позицию по у.
+            jl Col
+            mov N, 0d   // Иначе конец программы.
+            jmp Exit
+
+            Col :
+            mov eax, my     // Записываем в eax позицию по у максимального элемента.
+            mov ebx, absy   // Записываем в ebx позицию по у максимального элемента по модулю.
+            sub eax, ebx    // Вычитание для опрделения , в соседних ли столбцах.
+            cmp eax, 0      // Проверка знака, чтобы узнать, нужно ли брать модуль.
+            jg Plus
+            neg eax         // Модуль.
+            Plus :
+            cmp eax, 1  // if |ax| > 1, то конец программы. 
+            jg Exit
+            mov N, 1d   // Иначе вывод ответа и конец. 
+            jmp Exit
+            Exit :
+    }
+    cout << "max= " << ma << " mx= " << n - mx << " my= " << m - my << endl;
+    cout << "maxAbs= " << arr[n - absx][m - absy] << " mxAbs= " << n - absx << " myAbs= " << m - absy << endl;
+    cout << "N=" << N << endl;
+    cout << "S=" << S << endl;
+}
---
+++ avs lab3/lab3.cpp
@@ -0,0 +1,252 @@
+﻿#include <iostream> 
+#include <string>
+#include <sstream> 
+#include <cmath>
+#include <regex>
+using  namespace std;
+
+// Проверка строки, содержит ли она что то кроме цифр.
+bool checkStr(string s)
+{
+    regex num_expr("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$"); // Регулярки мои регулярочки.
+    if (regex_match(s, num_expr)) return true;
+    return false;
+}
+
+// Конвертировать строку в double. 
+double stringToNum(string s)
+{
+    double num;
+    istringstream buf(s);
+    buf >> dec >> num;
+    return num;
+}
+
+// Х, спец ограничений нет
+double getX(string x1)
+{
+    double x;
+    string s; // Вспомогательная.
+    bool ok = false;
+    do {
+        // Вводим строку. 
+        cout << "Введите " << x1 << ": ";
+        cin >> s;
+
+        // Проверка, введено ли число.
+        if (!checkStr(s))   
+        {
+            cout << "..\nОшибка 1: " << x1 << " должен быть числом\n\n\n";
+            continue;
+        }
+
+        // Конвертация в double и проверка, не равен ли Х нулю.
+        x = stringToNum(s);
+        if (x == 0)
+        {
+            cout << "..\nОшибка 2: X не должно быть 0\n\n\n";
+            continue;
+        }
+        ok = true;
+    } while (!ok);
+    return x;
+}
+
+// n, >0, целое.
+int getN()
+{
+    int n;
+    bool ok = false;
+    do {
+        // Вводим с помощью функции для Х.
+        double m = getX("N"); 
+
+        // Дополнительные проверки на "целое" и "положительное".
+
+        if (m - floor(m)) // Если не целое.
+        {
+            cout << "..\nОшибка 2: N должен быть целым числом\n\n\n";
+            continue;
+        }
+
+        if (m < 1)  // Если не положительное. 
+        {
+            cout << "..\nОшибка 3: N должен быть больше 0\n\n\n";
+            continue;
+        }
+
+        if (m > 1000)   // Если слишком большое.
+        {
+            cout << "..\nОшибка 3: N должен быть меньше 1000\n\n\n";
+            continue;
+        }
+        ok = true;
+        n = (int)m;
+    } while (!ok);
+    return n;
+}
+
+// Точность, >0
+double getE()
+{
+    double e;
+    bool ok = false;
+    do {
+        // Вводим с помощью функции для Х.
+        e = getX("eps");
+
+        if (e <= 0) // Если отрицательное. 
+        {
+            cout << "..\nОшибка 4: Eps должен быть больше 0\n\n\n";
+            continue;
+        }
+        if (abs((e > 99999)) || abs((e < 0.000001)))    // На диапазон.
+        {
+            cout << "..\nОшибка: eps не должен быть слишком маленьким или слишком большим числом\n\n\n";
+            continue;
+        }
+        ok = true;
+    } while (!ok);
+    return e;
+}
+
+
+// -ln(abs(2*sin(x/2)));
+double Task1(double x) {
+    double ans = 0, part1 = x, part2 = 0, numdiv = 2, one = 1, two = 2, minus = -1;
+    double t = 0;
+    __asm {
+        finit       // Инициализация.
+        fld one     // Опускаем в стек 1.
+        fld x       // Опускаем в стек х.
+        fld two     // Опускаем в стек 2.
+        fdiv        // Деление х на 2.
+        fsin        // sin x/2.
+        fmul two    // 2 * sin x/2.
+        fabs        // Модуль 2 * sin x/2.
+        
+        // log2(x) / log2(e)  =  ln(x)
+        
+        fyl2x       // Логарифм последнего значения, выгружаем из стека единицу. 
+        fldl2e      // Загружаем в стек log2(e)
+        fdiv        // Деление.
+        fmul minus  // Меняем знак.
+        fstp ans    // Сохраняем ответ в переменную ans.
+    }
+    return ans;
+}
+
+// Проверка на плюсах.
+double TestCPPTask1(double x) {
+    return -log(abs(2 * sin(x / 2)));
+}
+
+
+// cos(nx)/n
+double TestCPPTask2N(double x, int n)
+{
+    double ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        ans += cos(x * double(i)) / double(i);
+    }
+    return ans;
+}
+
+// cos(nx)/n
+// Sum(cos(x*n)/n, (n, 1, inf))
+double TestCPPTask2Eps(double x, double eps)
+{
+    int iter = 1;
+    double ans = 0, prev = 1000;
+    while (abs(prev - ans) > eps) {
+        prev = ans;
+        ans += cos(x * double(iter)) / double(iter);
+        iter++;
+    }
+    return ans;
+}
+
+// cos(nx)/n
+double Task2N(double x, int n)
+{
+    n++;
+    double ans = 0;
+    int iter = 1;   // Счетчик.
+    __asm {
+        mov ecx, 1  // i.        
+        calc :
+        finit               // Инициализация сопроцессора.
+            mov iter, ecx
+
+            fld x           // Загружаем х в стек.
+            fimul iter      // Целочисленное умножение х на счетчик.
+            fcos            // cos(nx).
+
+            fidiv iter      // cos(nx)/n.
+            fadd ans        // Вещественное сложение вершины стека с ans.
+            fstp ans        // Перезаписываем ans.
+            inc ecx         // i++.
+            cmp ecx, n      // if (ecx != n) => goto calc.
+            jne calc
+    }
+    return ans;
+}
+
+// cos(nx)/n
+double Task2Eps(double x, double eps)
+{
+    double ans = 0, tmpans, prev = 1000, dif = 0;
+    int iter = 1;
+    __asm {
+        mov ecx, 1 // i.
+        calc :
+        finit
+
+            mov iter, ecx
+            fld x       // Загружаем х в стек.
+            fimul iter  // Целочисленное умножение х на счетчик.
+            fcos        // cos(nx).
+
+            fidiv iter  // cos(nx)/n.
+            fstp tmpans // Перезаписываем tmpans.
+
+            fld ans     // Загружаем ans в стек.
+            fstp prev   // Сохраняем ans в prev.
+
+            fld tmpans  // Загружаем tmpans в стек.
+            fadd ans    // Вещественное сложение tmpans с ans.
+            fstp ans    // Перезаписываем ans. 
+
+            inc ecx     // i++.
+
+            fld ans     // Загружаем ans в стек.
+            fsub prev   // Вещественное вычитание (ans-prev).
+            fabs        // Модуль abs(ans-prev).
+            fstp dif    // Сохранение в dif.
+
+            fld dif     // Загружаем в стек dif.
+            fld eps     // Загружаем в стек Eps.
+            fcom st(1)  // Cравнение 
+            fstsw ax    // Сохранение содержимого регистра перед безусловным переходом. 
+            sahf        // Запись регистра во флаги для сравнения. 
+            jb calc     // goto calc.
+    }
+    return ans;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x = getX("x");
+    int n = getN();
+    double eps = getE();
+    cout << endl << "\Y cpp = \t" << TestCPPTask1(x) << endl;
+    cout << "Y asm = \t" << Task1(x) << endl << endl;
+
+    cout << "ограничение по N cpp = \t" << TestCPPTask2N(x, n) << endl;
+    cout << "ограничение по N asm = \t" << Task2N(x, n) << endl << endl;
+
+    cout << "ограничение по eps cpp = " << TestCPPTask2Eps(x, eps) << endl;
+    cout << "ограничение по eps asm = " << Task2Eps(x, eps) << endl;
+    return 0;
+}
